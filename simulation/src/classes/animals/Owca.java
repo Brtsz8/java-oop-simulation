af8df2e4d5a0d6a19ced3ec6b2b56007f6a7a430
Brtsz8 <bartoszpacyga06@gmail.com>
2025-05-16 17:25:06 +0200

    finished owca, files for all animals/plants

---
+++ simulation/src/classes/animals/Owca.java
@@ -0,0 +1,29 @@
+package classes.animals;
+
+import classes.Organizm;
+import classes.Swiat;
+import classes.Zwierze;
+
+public class Owca extends Zwierze {
+
+    //konstruktor
+    public Owca(int pozycjaX, int pozycjaY, Swiat swiat) {
+        //sila i inicjatywa = 4
+        super(4, 4, pozycjaX, pozycjaY, swiat);
+    }
+
+    @Override
+    public char rysowanie() {
+        return 'O';
+    }
+
+    @Override
+    public String nazwa() {
+        return "Owca";
+    }
+
+    @Override
+    public Organizm dodajPotomka(int x, int y) {
+         return new Owca(x,y,getSwiat());
+    }
+}
